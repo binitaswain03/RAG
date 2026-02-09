e5564c9933ce384457b9c97fdd86d0c0190a0f46
binitaswain03 <swainbinita30@gmail.com>
2026-02-09 23:29:18 +0530

    second

--- modules/verification.py
+++ modules/verification.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 import config
 
